35d216ff04a588f2a5e232ab5bb96d554c3a46ec
SLombardo158 <hopeskyfall36@gmail.com>
2025-10-09 20:40:10 -0500

    adding other references

--- portfolio/src/data/projects.ts
+++ portfolio/src/data/projects.ts
@@ -10,5 +10,5 @@ export type Project = {
 export const projects: Project[] = [
     { id: 1, title: "Drone Telemetry Visualizer", blurb: "Real-time multi-UAS map with alerts and playback.", stack: ["Python", "Unity", "C#", "SQL", "TCP", "UDP", "FastAPI"], href: "#" },
     { id: 2, title: "Setwave - Music Discovery Tool", blurb: "Audit KPIs, scheduled reports, role-based access.", stack: ["C#", "ASP.NET", "EF Core", "SQL", "Azure"], href: "#" },
-    { id: 3, title: "FCLWebVis - Data Visualization Framework", blurb: "Met one-on-one with leading Professor about vector manipulation and code base", stack: ["React", "Vector Math"], href: "https://www2.cs.uh.edu/~chengu/Publications/3DFlowVis/FCLWebVis_VDA23.pdf" }
+    { id: 3, title: "FCLWebVis - Data Visualization Framework", blurb: "Met one-on-one with leading Professor about vector manipulation and code base", stack: ["React", "Vector Math"], href: "https://www2.cs.uh.edu/~chengu/Publications/3DFlowVis/FCLWebVis_VDA23.pdf" } //other github href = https://github.com/MangoLion/FCLWebVis
 ];
